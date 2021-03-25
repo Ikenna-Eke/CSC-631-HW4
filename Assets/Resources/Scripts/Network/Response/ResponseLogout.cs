8444bd3588d75ec466ac91ce775ebda16dc89cf6
Valentine <shadow_hero@rocketmail.com>
2021-03-24 19:29:51 -0700

    Responses (login, logout, fireball, register)

---
+++ Assets/Resources/Scripts/Network/Response/ResponseLogout.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResponseLogout : ExtendedEventArgs
+{
+    public int user_id { get; set; } // Who is logging out
+
+	public ResponseLeaveEventArgs()
+	{
+		event_id = Constants.SMSG_LOGOUT;
+	}
+}
+
+public class ResponseLeaveResponse : NetworkResponse
+{
+
+}
