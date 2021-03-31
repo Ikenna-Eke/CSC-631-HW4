c4a80e1b3ee26217a8f277f5a0a4490df987efe1
wleung4 <50734742+wleung4@users.noreply.github.com>
2021-03-31 14:21:39 -0700

    attempt to get up to date

---
+++ Assets/Resources/Scripts/ServerClient/ServerClient/GameServer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GameServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Game Server";
+
+            Server.Start(2, 30033); // Start(max players, port)
+
+            Console.ReadKey();
+        }
+    }
+}
